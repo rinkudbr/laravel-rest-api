4eddc347e979b8973e6d4e1de2b3e8c150659c95
rinku-tandf <rinkubenh.c.dobariya@informa.com>
2022-06-12 00:56:10 +0530

    API with JWT token and test case

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,28 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\PropertyController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::post('login', [ApiController::class, 'authenticate']);
+Route::post('register', [ApiController::class, 'register']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::get('logout', [ApiController::class, 'logout']);
+    Route::get('get_user', [ApiController::class, 'get_user']);
+
+    Route::get('items', [ItemController::class, 'index']);
+    Route::get('item/{id}', [ItemController::class, 'show']);
+    Route::post('item', [ItemController::class, 'store']);
+    Route::put('item/{item}',  [ItemController::class, 'update']);
+    Route::delete('item/{item}',  [ItemController::class, 'destroy']);
+
+
+    Route::get('property', [PropertyController::class, 'index']);
+    Route::get('property/{id}', [PropertyController::class, 'show']);
+    Route::post('property', [PropertyController::class, 'store']);
+    Route::put('property/{property}',  [PropertyController::class, 'update']);
+    Route::delete('property/{property}',  [PropertyController::class, 'destroy']);
+
 });
